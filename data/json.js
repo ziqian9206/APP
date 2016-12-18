1d8a69cd5ca68394079be0b1adc57be6a63037aa
huxi <huxi.post@163.com>
2016-12-18 16:08:25 +0800

    app

---
+++ data/json.js
@@ -0,0 +1,16 @@
+var list = {
+	"success":true,
+	"data":"[
+		{
+			url:'',
+			name:"商品1",
+			oldprice: 33.20,
+			price:43.20
+		},{
+			"url":"",
+			"name":"商品1",
+			"oldprice": 33.20,
+			"price":43.20
+		}
+	]"
+}
